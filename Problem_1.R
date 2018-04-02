719d92263df9d2c54fc22b92f7ac5c663a05659b
karamfilovairena <37996414+karamfilovairena@users.noreply.github.com>
2018-04-02 11:49:36 +0300

    Add files via upload

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,19 @@
-
+#a
+x <- c( 4, 1, 1, 4)
+#b
+y <- c(1, 4)
+#c
+diff(x,y)
+# It is impossible for R to compute the difference between these two vectors, because they have a different length.
+#d
+s <- c(x, y)
+#e
+m <-rep(s,10)
+length(m)
+#f
+rep(s,each=3)
+#g
+seq(7,21, by=1)
+7:21
+#h
+length(7:21)
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,30 @@
-
+#a
+xmin <- c(23.0,20.5,28.2,20.3,22.4,17.2,18.2)
+xmax <- c( 25.0,22.8,31.2,27.3,28.4,20.2, 24.1)
+#b
+abs(xmax-xmin)
+#c
+avgMinTemp <- mean(xmin)
+avgMaxTemp <- mean (xmax)
+#d
+xmin[ xmin < avgMinTemp]
+#e
+xmax[xmax > avgMaxTemp]
+#f
+date <- c('03Mon18', '04Tue18', '05Wed18', '04Thu18', '05Fri18', '06Sat18', '07Sun18')
+names(xmin) <- date
+names(xmax) <- date
+#g
+temperatures <- data.frame(xmin= xmin, xmax=xmax)
+temperatures
+#h
+temperatures <- within(temperatures, {
+xminFahrenheit <- xmin*1.8 +32
+})
+temperatures
+#i
+tempFahreinheit <- data.frame(xminFahrenheit= xminFahrenheit, {
+  xmaxFahrenheint <- xmax*1.8 +32
+  })
+tempFahreinheit
+#j
